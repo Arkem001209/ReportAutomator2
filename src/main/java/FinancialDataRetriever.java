2cd6cae885a4ad8c58af0c22815e9304a00912ed
Max.Arkell <maxarkell@icloud.com>
2023-09-12 16:54:48 +0100

    Add code to disable http cookies to allow API call

--- src/main/java/FinancialDataRetriever.java
+++ src/main/java/FinancialDataRetriever.java
@@ -2,10 +2,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
 
 public class FinancialDataRetriever {
     private static final String XPLAN_API_URL = "https://londonwall.xplan.iress.co.uk/resourceful";
@@ -13,10 +13,15 @@ public class FinancialDataRetriever {
     private static final String USERNAME = "APIUser";
     private static final String PASSWORD = "WelcomeBack!1";
 
-//To do add correct URL, make sure that it works with 2FA
-
     public static JsonNode fetchFinancialData() throws Exception {
-        HttpClient httpClient = HttpClients.createDefault();
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setCookieSpec("ignore")
+                .build();
+
+        HttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
         HttpGet request = new HttpGet(XPLAN_API_URL);
 
         // Add headers for authorization
