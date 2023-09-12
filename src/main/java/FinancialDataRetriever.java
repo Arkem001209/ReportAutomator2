60e4441abe33b89507b0c6d2b855f108e715276c
Max.Arkell <maxarkell@icloud.com>
2023-09-12 14:02:45 +0100

    Alter the code to accept authorisation headers

--- src/main/java/FinancialDataRetriever.java
+++ src/main/java/FinancialDataRetriever.java
@@ -6,21 +6,23 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-import java.util.Base64;
-
 
 public class FinancialDataRetriever {
-    private static final String XPLAN_API_URL = "https://londonwall.xplan.iress.co.uk/resourceful/";
+    private static final String XPLAN_API_URL = "https://your-xplan-api-url/financial-data-endpoint";
+    private static final String API_KEY = "pGQsWd3FYdyJOnyno5Ng";
+    private static final String USERNAME = "APIUser";
+    private static final String PASSWORD = "WelcomeBack!1";
 
-    String apiKey = "pGQsWd3FYdyJOnyno5Ng";
-    String username = "APIUser";
-    String password = "WelcomeBack!1";
-    String credentials = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
 //To do add correct URL, make sure that it works with 2FA
 
     public static JsonNode fetchFinancialData() throws Exception {
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet request = new HttpGet(XPLAN_API_URL);
+
+        // Add headers for authorization
+        request.addHeader("Authorization", "Basic " + API_KEY);
+        request.addHeader("X-Username", USERNAME);
+        request.addHeader("X-Password", PASSWORD);
 
         HttpResponse response = httpClient.execute(request);
 
