a44423b14c199c25040ea5c04dd56ba61b7e0c9c
Max.Arkell <maxarkell@icloud.com>
2023-09-18 16:42:45 +0100

    Clean up comments and unnecessary code
    was getting crazy up in here

--- src/main/java/FinancialDataRetriever.java
+++ src/main/java/FinancialDataRetriever.java
@@ -1,6 +1,8 @@
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
@@ -20,60 +22,38 @@ public class FinancialDataRetriever {
         String valueToEncode = username + ":" + password;
         return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
     }
-    //public static JsonNode fetchFinancialData() throws Exception {
-        // Create an HTTP URL connection
-        //URL url = new URL(XPLAN_API_URL);
-        //HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        // Disable cookie handling
-        //connection.setRequestProperty("Cookie", "dummyCookie=1"); // Set a dummy cookie to disable automatic cookie handling
-        static JsonNode fetchFinancialData() throws Exception {
+    static JsonNode fetchFinancialData() throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request;
 
-    {
-        try {
-            request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(new URI(XPLAN_API_URL))
-                    .header("Authorization", getBasicAuthenticationHeader(USERNAME, PASSWORD))
-                    .header("X-Username", USERNAME)
-                    .header("X-Password", PASSWORD)
-                    .header("Cookie", "dummyCookie=1")
-                    .build();
+
+    try {
+        request = HttpRequest.newBuilder()
+                .GET()
+                .uri(new URI(XPLAN_API_URL))
+                .header("Authorization", getBasicAuthenticationHeader(USERNAME, PASSWORD))
+                .header("X-Username", USERNAME)
+                .header("X-Password", PASSWORD)
+                .header("Cookie", "dummyCookie=1")
+                .build();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-    }
+
     // Add headers for authorization
     HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
     HttpEntity entity = response.notify();
 
-    if (entity != null) {
-        String financialData = EntityUtils.toString(entity);
-        JSONObject json = new JSONObject(financialData);
+    int responseCode = response.statusCode();
+
+    if (responseCode == HttpURLConnection.HTTP_OK) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonResponse = objectMapper.readTree(response.getInputStream());
+
+    }
+            return jsonResponse;
     }
 
-    return response.body();
-    }
+}
 
-
-        //if (responseCode == HttpURLConnection.HTTP_OK) {
-            //ObjectMapper objectMapper = new ObjectMapper();
-            //JsonNode jsonResponse = objectMapper.readTree(connection.getInputStream());
-            //return jsonResponse;
-        //} //else {
-            //throw new Exception("Failed to fetch financial data from XPLAN API. Response code: " + responseCode);
-       // }
-
-    //public static void main(String[] args) {
-        //try {
-            //JsonNode financialData = fetchFinancialData();
-            // Process the retrieved financial data as needed
-            //System.out.println(financialData);
-        } //catch (Exception e) {
-           // e.printStackTrace();
-      // }
-  // }
-//}
 
