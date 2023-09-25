6f582df1dd5c0f77586c86b3eedf6fabbc47a2f9
Max.Arkell <maxarkell@icloud.com>
2023-09-25 17:12:13 +0100

    Enable both keys to get response code to work

--- src/main/java/FinancialDataRetriever.java
+++ src/main/java/FinancialDataRetriever.java
@@ -6,12 +6,13 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Base64;
 
 
 
 public class FinancialDataRetriever {
-    private static final String XPLAN_API_URL = "https://londonwall.xplan.iress.co.uk/resourceful";
+    private static final String XPLAN_API_URL = "https://londonwall.xplan.iress.co.uk/resourceful/";
     private static final String API_KEY = "pGQsWd3FYdyJOnyno5Ng";
     private static final String USERNAME = "APIUser";
     private static final String PASSWORD = "WelcomeBack!1";
@@ -29,8 +30,8 @@ public class FinancialDataRetriever {
         request = HttpRequest.newBuilder()
                 .GET()
                 .uri(new URI(XPLAN_API_URL))
-                //.header("Authorization", getBasicAuthenticationHeader(USERNAME, PASSWORD))
-                .header("Authorization", "Basic " + API_KEY)
+                .header("Authorization", getBasicAuthenticationHeader(USERNAME, PASSWORD))
+                .header("X-Xplan-App-id", API_KEY)
                 .header("X-Username", USERNAME)
                 .header("X-Password", PASSWORD)
                 .header("Cookie", "dummyCookie=1")
@@ -40,11 +41,13 @@ public class FinancialDataRetriever {
         }
 
     // Add headers for authorization
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
 
     int responseCode = response.statusCode();
+    String reasonPhrase = response.body();
         System.out.println(responseCode);
+        System.out.println(reasonPhrase);
 
     if (responseCode == 200) {
         String jsonResponse = response.body();
